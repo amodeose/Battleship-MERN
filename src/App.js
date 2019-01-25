98a3c7e4d224f45efabebfa1d5fc8daeafa00047
amodeose <amodeosa@gmail.com>
2019-01-25 11:09:40 -0500

    ship counter and appropriate restrictions placed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mouseDown: false
+      mouseDown: false,
+      one: 4,
+      two: 3,
+      three: 2,
+      four: 1,
+      count: 0,
+      memory: []
     }
   }
 
@@ -14,6 +20,10 @@ class App extends Component {
     e.preventDefault();
     if (e.target.classList.contains("tile")) {
       e.target.style.backgroundColor = "blue";
+      this.state.memory.push(e.target.id);
+      this.setState({
+        count: this.state.count + 1
+      })
     };
 
     this.setState({
@@ -22,17 +32,72 @@ class App extends Component {
   }
 
   handleMouseUp = () => {
+    const count = this.state.count;
+    switch(count) {
+      case 1:
+        if (this.state.one === 0) {
+          for (let i = 0; i < this.state.memory.length; i++) {
+            const tile = document.getElementById(this.state.memory[i]);
+            tile.style.backgroundColor = "white";
+          }
+          break;
+        }
+        this.setState ({
+          one: this.state.one - 1
+        });
+        break;
+      case 2:
+        if (this.state.two === 0) {
+          for (let i = 0; i < this.state.memory.length; i++) {
+            const tile = document.getElementById(this.state.memory[i]);
+            tile.style.backgroundColor = "white";
+          }
+          break;
+        }
+        this.setState ({
+          two: this.state.two - 1
+        });
+        break;
+      case 3:
+        if (this.state.three === 0) {
+          for (let i = 0; i < this.state.memory.length; i++) {
+            const tile = document.getElementById(this.state.memory[i]);
+            tile.style.backgroundColor = "white";
+          }
+          break;
+        }
+        this.setState ({
+          three: this.state.three - 1
+        });
+        break;
+      case 4:
+        if (this.state.four === 0) {
+          for (let i = 0; i < this.state.memory.length; i++) {
+            const tile = document.getElementById(this.state.memory[i]);
+            tile.style.backgroundColor = "white";
+          }
+          break;
+        }
+        this.setState ({
+          four: this.state.four - 1
+        });
+        break;
+      default:
+    }
     this.setState({
-      mouseDown: false
-    });
-    this.setState({
-      mouseDown: false
+      mouseDown: false,
+      count: 0,
+      memory: []
     });
   }
 
   handleMouseOver = (e) => {
-    if (this.state.mouseDown === true) {
+    if (this.state.mouseDown === true && this.state.count < 4) {
       e.target.style.backgroundColor = "blue";
+      this.state.memory.push(e.target.id);
+      this.setState({
+        count: this.state.count + 1
+      })
     }
   }
 
@@ -61,6 +126,31 @@ class App extends Component {
 
     return (
       <div className="App"  onMouseUp={this.handleMouseUp}>
+        <div className="shipCount">
+          {this.state.one > 0 && <div className="one">
+            <div className="tile blue"></div>
+            <h3>Remaining: {this.state.one}</h3>
+          </div>}
+          {this.state.two > 0 && <div className="two">
+            <div className="tile blue"></div>
+            <div className="tile blue"></div>
+            <h3>Remaining: {this.state.two}</h3>
+          </div>}
+          {this.state.three > 0 && <div className="three">
+            <div className="tile blue"></div>
+            <div className="tile blue"></div>
+            <div className="tile blue"></div>
+            <h3>Remaining: {this.state.three}</h3>
+          </div>}
+          {this.state.four > 0 && <div className="four">
+            <div className="tile blue"></div>
+            <div className="tile blue"></div>
+            <div className="tile blue"></div>
+            <div className="tile blue"></div>
+            <h3>Remaining: {this.state.four}</h3>
+          </div>}
+          
+        </div>
         <div className="player boundary" onMouseDown={this.handleMouseDown}>
           {generatePlayerGrid()}
         </div>
